d4629e087ba1c9473ba73f39763cd738aa724b48
hyson666666 <2854529108@qq.cpm>
2019-12-28 13:38:42 +0800

    字节码

---
+++ src/test/java/com/tx/tx_11_29/fanxing/FanxingTest.java
@@ -0,0 +1,14 @@
+package com.tx.tx_11_29.fanxing;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.Map;
+
+public class FanxingTest {
+
+    public static void main(String[] args) {
+        List<String> list = Lists.newArrayList("xxxxx");
+    }
+
+}
