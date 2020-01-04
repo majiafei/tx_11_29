2385984fe10266bf18861e0bf06b383fd749a3a3
fury36 <40300926+fury36@users.noreply.github.com>
2020-01-04 09:15:17 +0800

    日期格式化的bug

--- src/test/java/com/tx/tx_11_29/fanxing/FanxingTest.java
+++ src/test/java/com/tx/tx_11_29/fanxing/FanxingTest.java
@@ -2,13 +2,26 @@ package com.tx.tx_11_29.fanxing;
 
 import com.google.common.collect.Lists;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
 public class FanxingTest {
 
     public static void main(String[] args) {
-        List<String> list = Lists.newArrayList("xxxxx");
+        //List<String> list = Lists.newArrayList("xxxxx");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2019, Calendar.DECEMBER, 31);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+        // 2020-12-31
+        System.out.println(sdf.format(calendar.getTime()));
+
+        // 2019-12-31
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sdf1.format(calendar.getTime()));
+
     }
 
 }
