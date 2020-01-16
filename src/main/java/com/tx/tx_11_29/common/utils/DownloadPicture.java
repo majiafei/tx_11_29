1a5e7bb4daf7e1294692a1878f6b9e61387d8365
fury36 <40300926+fury36@users.noreply.github.com>
2020-01-16 11:43:57 +0800

    从远程服务器下载图片

---
+++ src/main/java/com/tx/tx_11_29/common/utils/DownloadPicture.java
@@ -0,0 +1,38 @@
+package com.tx.tx_11_29.common.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+/**
+ * @ClassName: DownloadPicture
+ * @Auther: admin
+ * @Date: 2020/1/16 11:40
+ * @Description:
+ */
+public class DownloadPicture {
+
+    public static void main(String[] args) {
+        URL url = null;
+        try {
+            url = new URL("https://ww2.sinaimg.cn/orj360/6defa4caly1gaxfecouszj20u0160dmk.jpg");
+            DataInputStream dataInputStream = new DataInputStream(url.openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("D:1.jpg"));
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = dataInputStream.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+            fileOutputStream.write(output.toByteArray());
+            dataInputStream.close();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
